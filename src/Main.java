1645b1173e6de2c2e350ed3337ac687093f6367d
klara-test <klarasindelarova@seznam.cz>
2024-07-23 20:11:57 +0200

    Words to guess are picked randomly. Added file words.txt with words to pick. If player loses, program prints the guessed word at the end.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -7,75 +9,92 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<String> wordsToGuess = new ArrayList<>();
 
-        System.out.println("Welcome to the game of Hanged man! Try guessing letters in the word, before the poor man gets hanged!");
+        try (Scanner reader = new Scanner(Paths.get("words.txt"))) {
+            while (reader.hasNextLine()) {
+                String row = reader.nextLine();
+                wordsToGuess.add(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error! " + e);
+        }
+        int index = 0;
+        Random randomNumber = new Random();
+        index = randomNumber.nextInt(wordsToGuess.size());
 
-        String finalWord = "HAMSTER";
+        String finalWord = wordsToGuess.get(index).toUpperCase();
         int mistakes = 0;
         List<String> letters = new ArrayList<>();
         List<String> spaces = new ArrayList<>();
+        List<String> usedLetters = new ArrayList<>();
         boolean victory = true;
         String[] parts = finalWord.split("(?!^)");
         for (int i = 0; i < finalWord.length(); i++) {
             letters.add(parts[i]);
             spaces.add("_");
         }
+            System.out.println("Welcome to the game of Hanged man! Try guessing letters in the word, before the poor man gets hanged!");
 
-        while (spaces.contains("_")) {
-            System.out.println("");
-            for (int i = 0; i < spaces.size(); i++) {
-                System.out.print(spaces.get(i) + " ");
-            }
-
-            System.out.println("");
-            if (mistakes == 0) {
-                printGallows();
-            }
-            if (mistakes == 1) {
-                printHead();
-            }
-            if (mistakes == 2) {
-                printTorso();
-            }
-            if (mistakes == 3) {
-                printRightHand();
-            }
-            if (mistakes == 4) {
-                printLeftHand();
-            }
-            if (mistakes == 5) {
-                printRightLeg();
-            }
-            if (mistakes == 6) {
-                printLeftLeg();
-                System.out.println("Oh no! Poor man!");
-                victory = false;
-                break;
-            }
-            System.out.println("Guess a letter: ");
-            String input = scanner.nextLine().toUpperCase().trim();
-            if (input.equals("")) {
-                continue;
-            }
-            boolean correct = false;
-            for (int i = 0; i < letters.size(); i++) {
-                if (letters.get(i).equals(input)) {
-                    spaces.set(i, input);
-                    correct = true;
+            while (spaces.contains("_")) {
+                System.out.println("");
+                for (int i = 0; i < spaces.size(); i++) {
+                    System.out.print(spaces.get(i) + " ");
                 }
-            }
-            if (correct == false) {
-                System.out.println("Wrong guess!");
-                mistakes = mistakes + 1;
+
+                System.out.println("");
+                if (mistakes == 0) {
+                    printGallows();
+                }
+                if (mistakes == 1) {
+                    printHead();
+                }
+                if (mistakes == 2) {
+                    printTorso();
+                }
+                if (mistakes == 3) {
+                    printRightHand();
+                }
+                if (mistakes == 4) {
+                    printLeftHand();
+                }
+                if (mistakes == 5) {
+                    printRightLeg();
+                }
+                if (mistakes == 6) {
+                    printLeftLeg();
+                    System.out.println("Oh no! Poor man!");
+                    System.out.println("The word was: " + finalWord);
+                    victory = false;
+                    break;
+                }
+                System.out.println("Already tried: " + usedLetters);
+                System.out.println("Guess a letter: ");
+                String input = scanner.nextLine().toUpperCase().trim();
+                if (input.equals("")) {
+                    continue;
+                }
+                usedLetters.add(input);
+                boolean correct = false;
+                for (int i = 0; i < letters.size(); i++) {
+                    if (letters.get(i).equals(input)) {
+                        spaces.set(i, input);
+                        correct = true;
+                    }
+                }
+                if (correct == false) {
+                    System.out.println("Wrong guess!");
+                    mistakes = mistakes + 1;
+                }
+
             }
 
-        }
+            if (victory == true) {
+                System.out.println("Good job! The man survived!");
+            }
 
-        if (victory == true) {
-            System.out.println("Good job! The man survived!");
-        }
+
     }
-
 
 
     public static void printGallows() {
@@ -98,7 +117,7 @@ public class Main {
                 "   |   \n" +
                 "   |    \n" +
                 "   |\n" +
-                "=============");
+                "=============\n");
         System.out.println("");
     }
 
@@ -110,7 +129,7 @@ public class Main {
                 "   |   \n" +
                 "   |    \n" +
                 "   |\n" +
-                "=============");
+                "=============\n");
         System.out.println("");
     }
 
@@ -122,7 +141,7 @@ public class Main {
                 "   |       \n" +
                 "   |    \n" +
                 "   |\n" +
-                "=============");
+                "=============\n");
         System.out.println("");
     }
 
@@ -134,7 +153,7 @@ public class Main {
                 "   |       \n" +
                 "   |    \n" +
                 "   |\n" +
-                "=============");
+                "=============\n");
         System.out.println("");
     }
 
@@ -146,7 +165,7 @@ public class Main {
                 "   |    /  \n" +
                 "   |    \n" +
                 "   |\n" +
-                "=============");
+                "=============\n");
         System.out.println("");
     }
 
@@ -158,7 +177,7 @@ public class Main {
                 "   |    / \\ \n" +
                 "   |    \n" +
                 "   |\n" +
-                "=============");
+                "=============\n");
         System.out.println("");
     }
 
